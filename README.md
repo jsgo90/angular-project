8adf8d5fb113f34e5eb33e14cef4a4196de0efde
jsgo90 <jsgarciaojeda@gmail.com>
2015-10-03 03:00:10 -0300

    + Added controllers loadHeaderTable and loadData. + Added RouterProvider + separate code in diferent views

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 <h1>Necro Prode project </h1>
 
 <h3> By JsGo90</h3>
-
---
+++ public/myControllers.js
@@ -0,0 +1,30 @@
+angular.module("myApp", ["ngRoute"])
+        .controller("loadData", function($scope) {
+          $scope.chars = [
+            {nombre:"Jon Snow 1", temporada:"1",capitulo:"1"},
+			{nombre:"Jon Snow 2", temporada:"2",capitulo:"2"},
+			{nombre:"Jon Snow 3", temporada:"3",capitulo:"3"},
+			{nombre:"Jon Snow 4", temporada:"4",capitulo:"4"},
+			{nombre:"Jon Snow 5", temporada:"5",capitulo:"5"},
+			{nombre:"Jon Snow 6", temporada:"6",capitulo:"6"},
+			{nombre:"Jon Snow 7", temporada:"7",capitulo:"7"}];
+          })
+		  .controller("loadHeaderTable", function($scope) {
+          $scope.columnas = [
+		  {name:'Nombre', temp:'Temporada', cap:'Capitulo',lnk:'Links'}];
+          })
+		  .constant("VIEWS", {
+          view01: "view/view01.html",
+          view02: "view/view02.html"
+		  })
+		  .config(["$routeProvider", "VIEWS", function($routeProvider, VIEWS) {
+          $routeProvider.when("/", {
+            templateUrl: VIEWS.view01
+          })
+		  .when("/view02", {
+            templateUrl: VIEWS.view02
+          })
+		  .otherwise({
+            redirectTo: "/"
+        });
+		}]);
