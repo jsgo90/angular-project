a2d3c48891f980b947a6032eb4342868c9193a45
jsgo90 <jsgarciaojeda@gmail.com>
2015-10-03 14:24:26 -0300

    more functions

--- public/myControllers.js
+++ public/myControllers.js
@@ -1,14 +1,28 @@
 angular.module("myApp", ["ngRoute"])
-        .controller("loadData", function($scope) {
+            .controller("loadData", ["$scope", function($scope) {
+                //Characters Array
           $scope.chars = [
             {nombre:"Jon Snow 1", temporada:"1",capitulo:"1"},
-			{nombre:"Jon Snow 2", temporada:"2",capitulo:"2"},
-			{nombre:"Jon Snow 3", temporada:"3",capitulo:"3"},
-			{nombre:"Jon Snow 4", temporada:"4",capitulo:"4"},
-			{nombre:"Jon Snow 5", temporada:"5",capitulo:"5"},
-			{nombre:"Jon Snow 6", temporada:"6",capitulo:"6"},
-			{nombre:"Jon Snow 7", temporada:"7",capitulo:"7"}];
-          })
+			      {nombre:"Jon Snow 2", temporada:"2",capitulo:"2"},
+			      {nombre:"Jon Snow 3", temporada:"3",capitulo:"3"},
+			      {nombre:"Jon Snow 4", temporada:"4",capitulo:"4"},
+		        {nombre:"Jon Snow 5", temporada:"5",capitulo:"5"},
+	          {nombre:"Jon Snow 6", temporada:"6",capitulo:"6"},
+			      {nombre:"Jon Snow 7", temporada:"7",capitulo:"7"}];
+
+              //New Character
+          $scope.newChar = function(){
+
+            $scope.chars.push({nombre: $scope.nombre,temporada: $scope.temporada,capitulo: $scope.capitulo});
+            $('#charRows').html($scope.chars);
+            //clear the fields after pushing the char
+            $scope.nombre = "";
+            $scope.temporada = "";
+            $scope.capitulo = "";
+          }
+
+        }])
+
 		  .controller("loadHeaderTable", function($scope) {
           $scope.columnas = [
 		  {name:'Nombre', temp:'Temporada', cap:'Capitulo',lnk:'Links'}];
